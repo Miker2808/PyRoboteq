0e090131fc9deca98ea0ceb712d6309854663b50
Mike <miker2808@gmail.com>
2020-08-13 12:29:38 +0300

    Major changes

--- PyRoboteq/roboteq_handler.py
+++ PyRoboteq/roboteq_handler.py
@@ -31,52 +31,12 @@ class RoboteqHandler:
             self.is_alive = True
 
         except Exception as e:
-            print("ERROR: Failed to connect to the roboteq device, read the exception error below..\n")
+            print("ERROR: Failed to connect to the roboteq device, read the exception error below:")
             print(e)
             print("\n\n")
             self.is_alive = False
         
         return self.is_alive
-
-    def dual_motor_control(self, left_motor: int = 0, right_motor: int = 0) -> None:
-        """
-        Controlling the motor using a Dual Drive mode
-        Send speed for the left, and right side of the robot/vehicle seperately 
-        Effective for doing Pivot drive and running track based robots
-        left_motor: integer from -1000 to 1000
-        right_motor: integer from -1000 to 1000
-        """
-        raw_command = "!M " + str(left_motor) + " " + str(right_motor) + "+\r"
-        try:
-            self.ser.write(raw_command.encode())
-        except Exception as e:
-            print("Failed to send command to the controller, read the exception error below..\n")
-            print(e)
-            print("\n\n")
-    
-    def send_param_command(self, command: str = "", first_argument: str = "", second_argument: str = "") -> None:
-        """
-        Send parameter based commands, this is the default method, using the roboteq_commands list you can choose a command
-        and write its arguments, at max there are just 2 arguments, using the manual, find out
-        how many arguments you need to use, those you dont need, just leave blank
-        """
-        
-        raw_command_0 = f"{command} {first_argument} {second_argument}+\r"
-        raw_command_1 = f"{command} {first_argument}+\r"
-        raw_command_2 = f"{command}+\r"
-        if first_argument == "" and second_argument == "":
-            raw_command = raw_command_2
-        elif first_argument is not "" and second_argument == "":
-            raw_command = raw_command_1
-        else:
-            raw_command = raw_command_0
-
-        try:
-            self.ser.write(raw_command.encode())
-        except Exception as e:
-            print("Failed to send command to the controller, read the exception error below..\n")
-            print(e)
-            print("\n\n")
 
     def send_raw_command(self, str_command: str = "") -> None:
         """
@@ -86,27 +46,48 @@ class RoboteqHandler:
         raw_command = f"{str_command}+\r"
         try:
             self.ser.write(raw_command.encode())
+
         except Exception as e:
-            print("Failed to send command to the controller, read the exception error below..\n")
+            print("Failed to send command to the controller, read the exception error below:")
             print(e)
             print("\n\n")
         
-
-    def read_value(self, command: str = "", argument: int = 1) -> str:
+    def request_handler(self, request: str = "") -> str:
         """
-        Reads value from the controller
+        Sends a command and a parameter, 
         """
         def get_data(serial):
             raw_data = b''
-            in_buffer = serial.inWaiting()
-            print(in_buffer)
             while raw_data == b'':
-                raw_data = serial.read_all().encode('utf-8')
-            data = raw_data.split("\r")
+                try:
+                    raw_data = serial.read_all()
+                except Exception as e:
+                    print("Failed to read from the controller, read the exception error below:")
+                    print(e)
+                    print("\n")
+                    raw_data = b' '
+            return raw_data
 
-            return data[1]
-
-        self.send_raw_command(f"{command} {argument}")
+        self.send_raw_command(request)
         result = get_data(self.ser)
+        result = result.decode()
+        result = result.split("\r")
 
-        return result
+        return result[1]
+
+    def dual_motor_control(self, left_motor: int = 0, right_motor: int = 0) -> None:
+        """
+        Controlling the motor using a Dual Drive mode
+        Send speed for the left, and right side of the robot/vehicle seperately 
+        Effective for doing Pivot drive and running track based robots
+        left_motor: integer from -1000 to 1000
+        right_motor: integer from -1000 to 1000
+        """
+        #raw_command = "!M " + str(left_motor) + " " + str(right_motor) + "\r"
+        raw_command = f"!M {left_motor} {right_motor} "
+        self.request_handler(raw_command)
+    
+    def read_value(self, command: str = "", parameter = "") -> str:
+        request = f"{command} [{parameter}]"
+        response = self.request_handler(request)
+        return response
